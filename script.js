580be2ede06ab4ff4f2e079ada17d854c74b652b
AhoaT <ahoa.tran@gmail.com>
2021-11-16 22:53:16 +0000

    Add buttons and math game random numbers

---
+++ script.js
@@ -0,0 +1,22 @@
+const targetNum = document.querySelectorAll(".target");
+const buttons = document.querySelectorAll("btn");
+
+
+//create a function that generates a 3 digit random number between 100 and 999
+function randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)+min);
+}
+
+console.log(randomNumber(100, 999));
+//Create arrays for the numbers that can be selected
+
+let largeNumber = [25, 50, 75, 100];
+let smallNumber = [1,2,3,4,5,6,7,8,9];
+let randomLargeNumber = Math.floor(Math.random() * largeNumber.length);
+let randomSmallNumber = Math.floor(Math.random() * smallNumber.length);
+console.log(largeNumber[randomLargeNumber]);
+console.log(smallNumber[randomSmallNumber]);
+
+//add event listener when clicked to generate random number in the output p tag
